234a9615e112c17b22c19f6007834f08c342d991
Mukhammadjon <kayumovmukhammadjon@gmail.com>
2022-07-01 23:29:19 +0500

    Adding Features

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,13 @@ import { KnexModule } from 'nest-knexjs';
 import config from './config/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { NewsModule } from './news/news.module';
+import { CategoryService } from './category/category.service';
+import { CategoryModule } from './category/category.module';
 
 @Module({
-  imports: [KnexModule.forRoot({ config: knexConfig[config.server.nodeEnv] })],
+  imports: [KnexModule.forRoot({ config: knexConfig[config.server.nodeEnv] }), NewsModule, CategoryModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, CategoryService],
 })
 export class AppModule {}
