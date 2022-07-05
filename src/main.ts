4f3da59cf615eb289366d3ac9995c654d5baac03
Mukhammadjon <kayumovmukhammadjon@gmail.com>
2022-07-05 09:17:11 +0500

    Adding Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import config from './config/config';
 
 async function bootstrap() {
@@ -11,6 +12,16 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Kun.uz API')
+    .setDescription('Kun.uz API description')
+    .setVersion('1.0')
+    .addTag('Kun.uz')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api-swagger', app, document);
 
   const { port } = config.server;
   await app.listen(port, () => {
