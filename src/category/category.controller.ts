0f38009fc5f8f82450d48d4ef20f9e334d56d726
Mukhammadjon <kayumovmukhammadjon@gmail.com>
2022-07-05 10:17:03 +0500

    Completed Category Api

--- src/category/category.controller.ts
+++ src/category/category.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Delete, Get, Inject, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Inject, Param, Post, Put, Query } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import {
   CreateCategoryDTO,
@@ -14,27 +14,27 @@ export class CategoryController {
   @Inject() private readonly categoryService: CategoryService;
 
   @Post()
-  async create(body: CreateCategoryDTO) {
+  async create(@Body() body: CreateCategoryDTO) {
     return await this.categoryService.create(body);
   }
 
   @Get()
-  async findAll(params: GetCategoryDTO) {
+  async findAll(@Query() params: GetCategoryDTO) {
     return await this.categoryService.findAll(params);
   }
 
   @Get('/:id')
-  async findOne(params: GetCategoryParamsDTO) {
+  async findOne(@Param() params: GetCategoryParamsDTO) {
     return await this.categoryService.findOne(params);
   }
 
   @Put('/:id')
-  async update(params: UpdateCategoryParamsDTO, body: UpdateCategoryDTO) {
+  async update(@Param() params: UpdateCategoryParamsDTO, @Body() body: UpdateCategoryDTO) {
     return await this.categoryService.update(params, body);
   }
 
   @Delete()
-  async delete(params: DeleteCategoryParamsDTO) {
+  async delete(@Body() params: DeleteCategoryParamsDTO) {
     return await this.categoryService.delete(params);
   }
 }
