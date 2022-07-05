b565e04506101199a7e12f1adc6f0e77601a4984
saad-usmon <saadcarnix012@gmail.com>
2022-07-05 12:23:29 +0500

    rating is added

---
+++ src/rating/dto/rating.dto.ts
@@ -0,0 +1,46 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Matches,
+} from 'class-validator';
+export class RatingDTO {
+  @ApiProperty()
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  news_id: string;
+
+  @ApiProperty()
+  @IsDefined()
+  @IsNotEmpty()
+  @IsString()
+  rating: string;
+}
+
+export class GetRatingDTO {
+  @ApiProperty()
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber()
+  @IsNotEmpty()
+  offset: number;
+
+  @ApiProperty()
+  @IsDefined()
+  @Type(() => Number)
+  @IsNumber()
+  @IsNotEmpty()
+  limit: number;
+}
+
+export class UpdateRatingDTO {
+  @ApiProperty()
+  @IsDefined()
+  @IsString()
+  @IsNotEmpty()
+  id: string;
+}
---
+++ src/rating/interfaces/rating.interface.ts
@@ -0,0 +1,6 @@
+interface IRating {
+  id: string;
+  created_at: Date;
+  news_id: string;
+  rating: string;
+}
